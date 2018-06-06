f7b94a71826d7b6c03f7d6381d0ef637556c247e
zlexdl <zlexdl@163.com>
2018-06-06 08:01:51 +0800

    add command -s

--- TokenTransfers/bin/EthSysWhaleHolder.sh
+++ TokenTransfers/bin/EthSysWhaleHolder.sh
@@ -8,7 +8,7 @@ cd /opt/Envs/py3/py3_project
 
 source /opt/Envs/py3/bin/activate
 
-scrapy crawl Ethsyswhaleholder -s DOWNLOAD_DELAY=60 LOG_FILE=/opt/Envs/py3/py3_project/logs/Ethsyswhaleholder_`date -d today +"%Y%m%d%H%M"`.log
+scrapy crawl Ethsyswhaleholder -s DOWNLOAD_DELAY=60 -s LOG_FILE=/opt/Envs/py3/py3_project/logs/Ethsyswhaleholder_`date -d today +"%Y%m%d%H%M"`.log
 
 
 
