31ec4221611e1e39e004ce3897d16ab5f4654a54
zlexdl <zlexdl@163.com>
2018-06-09 13:58:51 +0800

    追加自定义延迟时间

--- TokenTransfers/bin/CoinMarketCapHistory.sh
+++ TokenTransfers/bin/CoinMarketCapHistory.sh
@@ -5,7 +5,7 @@ cd /opt/Envs/py3/py3_project
 
 source /opt/Envs/py3/bin/activate
 
-scrapy crawl CoinMarketCapHistory -s LOG_FILE=/opt/Envs/py3/py3_project/logs/CoinMarketCapHistory_`date -d today +"%Y%m%d%H%M"`.log
+scrapy crawl CoinMarketCapHistory -s DOWNLOAD_DELAY=60 -s LOG_FILE=/opt/Envs/py3/py3_project/logs/CoinMarketCapHistory_`date -d today +"%Y%m%d%H%M"`.log
 
 
 
